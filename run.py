c8b98ae4152700ec7c22b9ad0ff85267a5d7ad0c
Jonas Krogh <36476478+Badgie@users.noreply.github.com>
2021-04-09 11:52:07 +0200

    Add argument parser (#8)
    * add argparse
    
    * oops
    
    * implement sample arg
    
    * implement scrape skip
    
    * update readme
    
    * add defaults to readme

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
 from data_handler import DataHandler
+from argparse import ArgumentParser
+
+arg_parser = ArgumentParser()
+arg_parser.add_argument('--sample', '-s', nargs='?', metavar='SIZE', type=int, default=0,
+                        help='sample size, default: 0 (full run)')
+arg_parser.add_argument('--no-scrape', '-ns', action='store_true',
+                        help='skip scraping during run')
+
+args = arg_parser.parse_args()
 
 if __name__ == '__main__':
-    dh = DataHandler()
+    dh = DataHandler(
+        sample=args.sample,
+        no_scrape=args.no_scrape
+    )
     dh.collect()
