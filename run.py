86daaf9f41d50fe57ba421a4e3a9220dce6f1c6f
Badgy <jonaskhansen@gmail.com>
2021-04-19 10:03:02 +0200

    add file arg

--- run.py
+++ run.py
@@ -9,12 +9,15 @@ arg_parser.add_argument('--no-scrape', '-ns', action='store_true',
                         help='skip scraping during run')
 arg_parser.add_argument('--push', '-p', action='store_true', 
                         help='push output to rust server')
+arg_parser.add_argument('--file', '-f', nargs=1, type=str,
+                        help='file path for xml to use (default: get from fødevarestyrelsen)')
 args = arg_parser.parse_args()
 
 if __name__ == '__main__':
     dh = DataHandler(
         sample=args.sample,
         no_scrape=args.no_scrape,
-        push=args.push
+        push=args.push,
+        file=args.file
     )
     dh.collect()
