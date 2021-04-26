d28128db03e06d9f0e5f4d4cc0099989f65c590e
Badgy <jonaskhansen@gmail.com>
2021-04-26 12:52:57 +0200

    fix stupid tests maybe

--- test/test_prev_processed_file.py
+++ test/test_prev_processed_file.py
@@ -1,4 +1,5 @@
 from filter_xml.prev_processed_file import PrevProcessedFile
+from filter_xml.catalog import Restaurant, SmileyReport
 import unittest
 import os
 
@@ -28,43 +29,50 @@ class PrevProcessedFileTest(unittest.TestCase):
 
     def test_should_not_process_restaurant_with_no_changes(self):
         seq_nr = '123'
-        self.create_file_with_data(seq_nr, '2001-01-01T00:00:00Z')
+        date = '2001-01-01T00:00:00Z'
+        self.create_file_with_data(seq_nr, date)
+        res = Restaurant()
+        res.name_seq_nr = '123'
+        res.smiley_reports = [SmileyReport.from_xml('1', date)]
 
         processed_file = PrevProcessedFile(FILENAME)
-        should_process = processed_file.should_process_restaurant(
-            {'navnelbnr': seq_nr, 'seneste_kontrol_dato': '01-01-2001 00:00:00'})
+        should_process = processed_file.should_process_restaurant(res)
 
         self.assertFalse(should_process)
 
     def test_should_process_new_restaurant(self):
         self.create_file_with_data()
+        res = Restaurant()
+        res.name_seq_nr = '555'
+        res.smiley_reports = [SmileyReport.from_xml('1', '2001-01-01T00:00:00Z')]
 
         processed_file = PrevProcessedFile(FILENAME)
-        should_process = processed_file.should_process_restaurant({
-            'navnelbnr': '555', 'seneste_kontrol_dato': '01-01-2001 00:00:00'})
+        should_process = processed_file.should_process_restaurant(res)
 
         self.assertTrue(should_process)
 
     def test_should_process_restaurant_with_new_date(self):
         seq_nr = '123'
         self.create_file_with_data(seq_nr, '2001-01-01T00:00:00Z')
+        res = Restaurant()
+        res.name_seq_nr = seq_nr
+        res.smiley_reports = [SmileyReport.from_xml('1', '2001-02-01T00:00:00Z')]
 
         processed_file = PrevProcessedFile(FILENAME)
-        should_process = processed_file.should_process_restaurant({
-            'navnelbnr': seq_nr, 'seneste_kontrol_dato': '01-02-2001 00:00:00'})
+        should_process = processed_file.should_process_restaurant(res)
 
         self.assertTrue(should_process)
 
     def test_date_in_file_gets_updated(self):
         seq_nr = '123'
         self.create_file_with_data(seq_nr, '2001-01-01T00:00:00Z')
-        new_restaurant = {'navnelbnr': seq_nr,
-                          'smiley_reports': [{'date': '2001-02-01T00:00:00Z'}]}
+        new_res = Restaurant()
+        new_res.name_seq_nr = seq_nr
+        new_res.smiley_reports = [SmileyReport.from_xml('1', '2001-02-01T00:00:00Z')]
 
         processed_file = PrevProcessedFile(FILENAME)
-        processed_file.should_process_restaurant(
-            {'navnelbnr': seq_nr, 'seneste_kontrol_dato': '01-02-2001 00:00:00'})
-        processed_file.add_list([new_restaurant])
+        processed_file.should_process_restaurant(new_res)
+        processed_file.add_list([new_res])
         processed_file.output_processed_companies()
         processed_file = PrevProcessedFile(FILENAME)
 
@@ -74,11 +82,12 @@ class PrevProcessedFileTest(unittest.TestCase):
     def test_old_entry_is_deleted_when_new_control_date(self):
         seq_nr = '123'
         self.create_file_with_data(seq_nr, '2001-01-01T00:00:00Z')
-        new_restaurant = {'navnelbnr': seq_nr,
-                          'seneste_kontrol_dato': '01-02-2001 00:00:00'}
+        new_res = Restaurant()
+        new_res.name_seq_nr = seq_nr
+        new_res.smiley_reports = [SmileyReport.from_xml('1', '2001-02-01T00:00:00Z')]
 
         processed_file = PrevProcessedFile(FILENAME)
-        processed_file.should_process_restaurant(new_restaurant)
+        processed_file.should_process_restaurant(new_res)
         processed_file.output_processed_companies()
         processed_file = PrevProcessedFile(FILENAME)
 
@@ -86,19 +95,25 @@ class PrevProcessedFileTest(unittest.TestCase):
             processed_file.get_control_date_by_seq_nr(seq_nr), None)
 
     def test_error_when_adding_restaurant_without_control_date(self):
-        restaurant = {'navnelbnr': '1111'}
+        restaurant = Restaurant()
+        restaurant.name_seq_nr = '1111'
         processed_file = PrevProcessedFile(FILENAME)
 
         with self.assertRaises(KeyError):
             processed_file.add_list([restaurant])
 
     def create_file_with_data(self, seq_nr='123', date='2001-01-01T00:00:00Z'):
-        restaurant1 = {'navnelbnr': '11111',
-                       'smiley_reports': [{'date': '2002-02-02T00:00:00Z'}]}
-        restaurant2 = {'navnelbnr': seq_nr,
-                       'smiley_reports': [{'date': date}]}
-        restaurant3 = {'navnelbnr': '22222',
-                       'smiley_reports': [{'date': '2003-03-03T00:00:00Z'}]}
+        restaurant1 = Restaurant()
+        restaurant1.name_seq_nr = '11111'
+        restaurant1.smiley_reports = [SmileyReport.from_xml('1', '2002-02-02T00:00:00Z')]
+
+        restaurant2 = Restaurant()
+        restaurant2.name_seq_nr = seq_nr
+        restaurant2.smiley_reports = [SmileyReport.from_xml('1', date)]
+
+        restaurant3 = Restaurant()
+        restaurant3.name_seq_nr = '22222'
+        restaurant3.smiley_reports = [SmileyReport.from_xml('1', '2003-03-03T00:00:00Z')]
 
         processed_file = PrevProcessedFile(FILENAME)
         processed_file.add_list([restaurant1, restaurant2, restaurant3])
