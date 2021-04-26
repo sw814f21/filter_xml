65c8548eed138c5577a694ecd076b11c4621eb6a
Badgy <jonaskhansen@gmail.com>
2021-04-26 13:01:40 +0200

    fix remaining stupid tests maybe

--- test/test_temp_file.py
+++ test/test_temp_file.py
@@ -2,6 +2,7 @@ import unittest
 import os
 
 from filter_xml.temp_file import TempFile
+from filter_xml.catalog import Restaurant
 
 
 class TempFileTest(unittest.TestCase):
@@ -15,24 +16,28 @@ class TempFileTest(unittest.TestCase):
 
     def test_can_add_valid_date(self):
         self.temp_file = TempFile()
+        data = Restaurant()
+        data.name_seq_nr = '4321'
 
-        self.temp_file.add_data(
-            {'navnelbnr': '4321', 'smiley_reports': 'testValue'})
+        self.temp_file.add_data(data)
 
     def test_get_all_correct_data(self):
         self.temp_file = TempFile()
-        row1 = {'navnelbnr': '123', 'smiley_reports': 'test1'}
-        row2 = {'navnelbnr': '456', 'smiley_reports': 'test2'}
-        row3 = {'navnelbnr': '789', 'smiley_reports': 'test3'}
+        row1 = Restaurant()
+        row1.name_seq_nr = '123'
+        row2 = Restaurant()
+        row2.name_seq_nr = '456'
+        row3 = Restaurant()
+        row3.name_seq_nr = '789'
         self.temp_file.add_data(row1)
         self.temp_file.add_data(row2)
         self.temp_file.add_data(row3)
 
         data = self.temp_file.get_all()
 
-        self.assertEqual(data[0], row1)
-        self.assertEqual(data[1], row2)
-        self.assertEqual(data[2], row3)
+        self.assertEqual(data.catalog[0], row1)
+        self.assertEqual(data.catalog[1], row2)
+        self.assertEqual(data.catalog[2], row3)
 
     def test_close_removes_file(self):
         self.temp_file = TempFile()
@@ -42,8 +47,9 @@ class TempFileTest(unittest.TestCase):
         self.assertFalse(os.path.exists(TempFile.FILE_NAME))
 
     def test_exception_when_no_seq_nr_in_data(self):
-        row1 = {'id': '123', 'field1': '321'}
+        row = Restaurant()
+        row.name_seq_nr = None
         self.temp_file = TempFile()
 
         with self.assertRaises(ValueError):
-            self.temp_file.add_data(row1)
+            self.temp_file.add_data(row)
