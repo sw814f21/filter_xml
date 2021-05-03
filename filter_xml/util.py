7709250ab100adbd7420b0b631ebf4665e7768a3
Badgy <jonaskhansen@gmail.com>
2021-05-03 11:31:36 +0200

    fix

--- filter_xml/util.py
+++ filter_xml/util.py
@@ -4,15 +4,15 @@ from datetime import datetime
 from pathlib import Path
 
 
-def is_file_from_today(path: str) -> bool:
+def is_file_old(path: str) -> bool:
     """
     Check whether or not a file defined by :param path is modified today
     """
     if not os.path.isfile(path):
-        return False
+        return True
 
     f_stat = Path(path).stat()
     m_stamp = datetime.fromtimestamp(f_stat.st_mtime)
     now = datetime.now()
 
-    return now.date() == m_stamp.date()
+    return now.date() != m_stamp.date()
