133bcac0760b6a2d6f79da26f89467effc9ee98a
Nicklas <nicklas.hug@gmail.com>
2021-05-05 10:26:58 +0200

    Temp file outputs json
    To be able to output and input None to fields in tempfile

--- filter_xml/temp_file.py
+++ filter_xml/temp_file.py
@@ -7,35 +7,23 @@ from filter_xml.catalog import Restaurant, RestaurantCatalog
 
 class TempFile:
     """
-    Handler for temp.csv file.
+    Handler for temp.json file.
 
     This file is maintained during a session, and is deleted once the session ends. This file
     ensures that we have saved progress in the case of a crash, meaning that this file is in
     charge of maintaining the state of the data collection at any given time. A session does not
     end until every row has been processed.
     """
-    FILE_NAME = "temp.csv"
+    FILE_NAME = "temp.json"
 
     def __init__(self) -> None:
         file_exists = os.path.exists(self.FILE_NAME)
-        read_append = 'r+' if file_exists else 'a+'
-        field_names = list(vars(Restaurant()).keys())
 
         self.__data = dict()
-        self.__file = open(self.FILE_NAME, read_append)
-        self.__file_writer = self.__get_file_writer(field_names)
 
         if file_exists:
             self.__data = self.__read_temp_data()
-        else:
-            self.__create_file()
 
-    def __create_file(self) -> None:
-        """
-        Create temp file with headers from keys in provided data_example
-        """
-        self.__file_writer.writeheader()
-        self.__file.flush()
 
     def __get_file_writer(self, field_names: list) -> csv.DictWriter:
         """
@@ -46,34 +34,43 @@ class TempFile:
             raise ValueError(
                 'Expected "name_seq_nr" field to be provided in data example')
 
-        return csv.DictWriter(
-            self.__file, fieldnames=field_names, extrasaction='ignore', delimiter=";")
+        return csv.writer(self.__file, delimiter=";")
 
     def __read_temp_data(self) -> dict:
         """
         Read temp file as a dictionary, indexed by p-number
         """
-        reader = csv.DictReader(self.__file, delimiter=";")
+
         out = dict()
-        for entry in reader:
-            entry['smiley_reports'] = json.loads(entry['smiley_reports'])
-            entry = Restaurant.from_json(entry)
-            out[entry.name_seq_nr] = entry
+
+        with open(self.FILE_NAME) as json_file:
+            data = json.load(json_file)
+            
+            for entry in data:
+                restaurant = Restaurant.from_json(entry)
+                out[restaurant.name_seq_nr] = restaurant
+
         return out
+
 
     def add_data(self, data: Restaurant):
         """
         Write a single row to temp file and commit
         """
         if not data.name_seq_nr:
-            self.close()
             raise ValueError('Expected data to have "name_seq_nr" key')
 
         self.__data[data.name_seq_nr] = data
-        data = data.as_dict().copy()
-        data['smiley_reports'] = json.dumps(data['smiley_reports'])
-        self.__file_writer.writerow(data)
-        self.__file.flush()
+
+        data_to_dump = []
+        for restaurant in self.__data.values():
+            data_to_dump.append(restaurant.as_dict())
+        
+        self._write_json(data_to_dump)
+
+    def _write_json(self, data: list):
+        with open(self.FILE_NAME, 'w') as json_file:
+            json.dump(data, json_file)
 
     def close(self) -> None:
         """
